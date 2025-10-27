45bc99e18443d283c7964b01b8b46d6136b8d6e5
leidZhang <113314347+leidZhang@users.noreply.github.com>
2025-10-27 12:09:13 -0400

    Add files via upload

---
+++ parse_config.py
@@ -0,0 +1,27 @@
+import sys
+from typing import List
+
+import yaml
+
+if __name__ == "__main__":
+    path = sys.path[0]
+    
+    with open(f"{path}/configs.yaml", "r") as file:
+        config: dict = yaml.safe_load(file)
+
+    base_dir_host: str = config.get("base_dir_host", "")
+    mount_dirs: List[str] = config.get("mount_dirs", [])
+    container_name: str = config.get("container_name", "")
+    base_dir_container: str = config.get("base_dir_container", "")
+
+    mounts: List[str] = []
+    for dir_name in mount_dirs:
+        host_dir: str = f"{base_dir_host}/{dir_name}"
+        docker_dir: str = f"{base_dir_container}/{dir_name}"
+        mounts.append(f"{host_dir}:{docker_dir}")
+
+    image_id: str = config.get("image_id", "")
+    print(f'MOUNT_DIRS="{ " ".join(mounts) }"')
+    print(f'IMAGE_ID="{image_id}"')
+    print(f'CONTAINER_NAME="{container_name}"')
+    
---
+++ run_container.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+eval "$(python3 $SCRIPT_DIR/parse_config.py)"
+echo "Using image ID: $IMAGE_ID"
+echo "Got mounted paths: $MOUNT_DIRS"
+echo "Using container name: $CONTAINER_NAME"
+
+# Split MOUNT_DIRS into an array
+IFS=' ' read -r -a mount_dirs <<< "$MOUNT_DIRS"
+# Iterate over the array
+for dir in "${mount_dirs[@]}"; do
+    echo "Mounting directory: $dir"
+    MOUNT_ARGS+=("-v" "$dir")
+done
+
+echo "Configuration complete. Starting attempting to container..."
+# Check if a container with the same name is already running
+if [ "$(docker ps -aq -f name="$CONTAINER_NAME")" ]; then
+    echo "Container with name $CONTAINER_NAME already exists. "
+    # Check if the container is running
+    if [ "$(docker ps -q -f name="$CONTAINER_NAME")" ]; then
+        echo "Container is already running. Attaching to it..."
+        docker exec -it "$CONTAINER_NAME" bash
+    else
+        echo "Starting existing container..."
+        docker start -ai "$CONTAINER_NAME"
+    fi
+else
+    # No existing container found, create a new one
+    echo "No existing container found. Creating a new one..."
+    docker run -it \
+    "${MOUNT_ARGS[@]}" \
+    --network host \
+    --name "$CONTAINER_NAME" \
+    "$IMAGE_ID" \
+    bash
+fi
